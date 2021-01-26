59f6b8e749aee5bbf519c9939d9b36d4dcbbd0ae
Parker Lawrence <parker@Parkers-MacBook-Pro-2.local>
2021-01-26 14:07:55 -0500

    main menu

--- scriptes/Menu_Control.gd
+++ scriptes/Menu_Control.gd
@@ -7,21 +7,23 @@
 extends Control
 
 
-func _ready():
-	# Center the windows in the screen
-	OS.window_position = (OS.get_screen_size()*0.5 - OS.window_size*0.5)
-
-
 func _on_Button_Exit_pressed():
 	# Exit the game
 	get_tree().quit()
 
 
+func _on_Button_Start_pressed():
+	pass
+
 func _on_Button_Settings_pressed():
 	# Show the Option Menu
-	get_node("Option_Control").show()
-
-
-func _on_Button_Start_pressed():
-	# Load the sceance here
+	#get_node("Option_Control").show()
 	pass
+
+
+func _on_Menu_Controller_ready():
+	#Centers the menu
+	OS.window_position = (OS.get_screen_size()*0.5 - OS.window_size*0.5)
+
+
+
