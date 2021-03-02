d4684f8323bedbbee5caf77798e2a3083a0d15f3
Chen Kai Chuang <chuang23@purdue.edu>
2021-03-02 17:19:22 -0500

    rotatingObject allows the object to set its actual (in engine) rotation

--- Client/objects/car.gd
+++ Client/objects/car.gd
@@ -1,7 +1,10 @@
 extends "res://objects/object.gd"
 
+func _ready():
+	currentRotation = -PI/2;
+func _control():
+	pass;
 #controls, movement
-
 
 #selecting which car animation spritesheet to use (which car type)
 
