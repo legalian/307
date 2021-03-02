3ba8b576f38122e1ca44ab738d941597e36f770f
Chen Kai Chuang <chuang23@purdue.edu>
2021-03-02 17:30:09 -0500

    Revert "rotatingObject allows the object to set its actual (in engine) rotation"
    This reverts commit d4684f8323bedbbee5caf77798e2a3083a0d15f3.

--- Client/objects/car.gd
+++ Client/objects/car.gd
@@ -1,10 +1,7 @@
 extends "res://objects/object.gd"
 
-func _ready():
-	currentRotation = -PI/2;
-func _control():
-	pass;
 #controls, movement
+
 
 #selecting which car animation spritesheet to use (which car type)
 
--- Client/rotatedObject.gd
+++ Client/rotatedObject.gd
@@ -2,18 +2,17 @@ tool
 extends Node2D
 
 
-var currentRotation = 0;
-
 func _ready():
 	set_process(true)
 
 func _process(delta):
 	#set_rotation(get_rotation() + delta * 5)
 	var glt = get_global_transform_with_canvas()
-	var gltp = atan2(glt[0][1]/.44,glt[0][0]) + currentRotation;
+	var gltp = atan2(glt[0][1]/.44,glt[0][0])
 	get_node("Visible/Sprite").frame = 63-(int(64+64*gltp/(2*PI))%64);
 	
 	glt.origin = Vector2(0,0);
 	var invscale = 1/(glt[0][0]*glt[1][1]-glt[1][0]*glt[0][1])
 	glt = glt.scaled(Vector2(invscale,invscale))
 	get_node("Visible").transform = glt.affine_inverse();
+
