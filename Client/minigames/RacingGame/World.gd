e799e0c564ab521466be600c40a082bdec02ccb5
Chen Kai Chuang <chuang23@purdue.edu>
2021-03-03 11:40:55 -0500

    fixed fence background, added fences, tested fences

--- Client/minigames/RacingGame/World.gd
+++ Client/minigames/RacingGame/World.gd
@@ -19,9 +19,8 @@ func _ready():
 	camera.current = true
 	#get_viewport().canvas_transform = get_viewport().canvas_transform.scaled(Vector2(2,1))
 	
-	object_scenes[Object_ids.FENCE] = preload("res://minigames/RacingGame/objects/fence.tscn")
-	object_scenes[Object_ids.CAR] = preload("res://minigames/RacingGame/objects/racingCar.tscn")
 	object_scenes[Object_ids.FENCE2] = preload("res://minigames/RacingGame/objects/fence.tscn")
+	object_scenes[Object_ids.CAR] = preload("res://minigames/RacingGame/objects/racingCar.tscn")
 	
 	
 	object_map = find_node("Objects")
