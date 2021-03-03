a3d92214fec02998b48ae0da9ea251d65e08f27f
Parker Lawrence <liverspleenkidney@gmail.com>
2021-03-02 23:27:03 -0500

    added servermap creation script

--- Server/BattleRoyale/World.gd
+++ Server/BattleRoyale/World.gd
@@ -4,9 +4,18 @@ func systemname():
 
 
 
+
+func add_player(newplayer):
+	.add_player(newplayer)
+
+
+
+
+
 remote func shoot():
 	var player_id = get_tree().get_rpc_sender_id()
 	print(str(player_id)+" is shooting.")
+
 
 
 
---
+++ Server/RacingGame/World.gd
@@ -0,0 +1,5 @@
+extends "res://GameBase.gd"
+func systemname():
+	return "RacingGame"
+
+
