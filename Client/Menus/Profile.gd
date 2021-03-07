157a584ad68207e93db803b8f69db8fa5ea65b70
WilliamNewt <40041126+WilliamNewt@users.noreply.github.com>
2021-03-07 16:03:25 -0500

    Remade character customization

--- Client/Menus/Profile.gd
+++ Client/Menus/Profile.gd
@@ -6,6 +6,8 @@ var AvatarSelected = 0
 onready var generalserver = get_node("/root/Server")
 var AvatarMenuOpen = false
 
+var AvatarStyles = ["Racoon"]
+
 func _MUT_send_partycode():
 	var partycode = $PartyCode.text
 	var file = File.new()
@@ -48,28 +50,34 @@ func _on_ChangeAvatar_pressed(AvatarType):
 	AvatarSelected = AvatarType
 	get_node("CurrentAvatar").text = "Avatar - " + str(AvatarSelected)
 
+func _on_Button_ChooseHat_pressed():
+	pass
 
 func _on_Button_ChooseCharacter_pressed():
 	if (AvatarMenuOpen):
-		get_node("Button_ChooseCharacter").text = "Change"
+		#get_node("Button_ChooseCharacter").text = "Change"
 		get_node("Avatar Menu").hide()
 		AvatarMenuOpen = false
 	else:
-		get_node("Button_ChooseCharacter").text = "Close"
+		#get_node("Button_ChooseCharacter").text = "Close"
 		get_node("Avatar Menu").show()
+		_set_Avatar_Selection()
 		AvatarMenuOpen = true
 
+func _set_Avatar_Selection():
+	var AvatarSelection = get_node("Avatar Menu").get_node("ColorRect").get_node("ButtonSelection")
+	var currentNode = AvatarSelection
+	for i in 8:
+		currentNode = AvatarSelection.get_node("Avatar" + str(i))
+		if(i < AvatarStyles.size()):
+			currentNode.get_node("AvatarType").text = AvatarStyles[i]
+			currentNode.show()
+		else:
+			currentNode.get_node("AvatarType").text = "Null"
+			currentNode.hide()
+			
+		
 
 func _on_EnterGameButton_pressed():
 	get_tree().change_scene("res://minigames/PartyScreen/LoadingScreen.tscn")
 	generalserver.attemptEnterGame()
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
