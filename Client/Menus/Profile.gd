ca3bac7dafb73542147338c4b816df3168106350
WilliamNewt <40041126+WilliamNewt@users.noreply.github.com>
2021-03-07 18:10:07 -0500

    Created Hat Menu
    Hat displays are not set and it does not set on character yet.

--- Client/Menus/Profile.gd
+++ Client/Menus/Profile.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 var UsernameInput = ""
 var AvatarSelected = 0
+var HatSelected = 0
 
 onready var generalserver = get_node("/root/Server")
 var AvatarMenuOpen = false
+var HatMenuOpen = false
 
 var AvatarStyles = ["Racoon"]
+var HatStyles = ["None","Tophat","Smallhat","Viking","Paperhat","Headphones"]
 
 func _MUT_send_partycode():
 	var partycode = $PartyCode.text
@@ -48,10 +51,36 @@ func _on_Button_ConfirmUsername_pressed():
 func _on_ChangeAvatar_pressed(AvatarType):
 	print(AvatarType)
 	AvatarSelected = AvatarType
-	get_node("CurrentAvatar").text = "Avatar - " + str(AvatarSelected)
+	get_node("CurrentAvatar").text = "Avatar - " + AvatarStyles[AvatarSelected]
+
+func _on_ChangeHat_pressed(HatType):
+	print(HatType)
+	HatSelected = HatType
+	get_node("CurrentHat").text = "Hat - " + HatStyles[HatSelected]
 
 func _on_Button_ChooseHat_pressed():
-	pass
+	if (HatMenuOpen):
+		#get_node("Button_ChooseCharacter").text = "Change"
+		get_node("Hat Menu").hide()
+		HatMenuOpen = false
+	else:
+		#get_node("Button_ChooseCharacter").text = "Close"
+		get_node("Hat Menu").show()
+		_set_Hat_Selection()
+		HatMenuOpen = true
+
+func _set_Hat_Selection():
+	var HatSelection = get_node("Hat Menu").get_node("ColorRect").get_node("ButtonSelection")
+	var currentNode = HatSelection
+	for i in 8:
+		currentNode = HatSelection.get_node("Avatar" + str(i))
+		if(i < HatStyles.size()):
+			currentNode.get_node("AvatarType").text = HatStyles[i]
+			currentNode.show()
+		else:
+			currentNode.get_node("AvatarType").text = "Null"
+			currentNode.hide()
+			
 
 func _on_Button_ChooseCharacter_pressed():
 	if (AvatarMenuOpen):
