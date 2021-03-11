f0c3e5d3def448ce9410495ca2783bf77e8d60a2
evantygf <evan.dunning@gmail.com>
2021-03-11 01:54:24 -0500

    Added countdown animation

--- Client/minigames/RacingGame/Countdown.gd
+++ Client/minigames/RacingGame/Countdown.gd
@@ -2,14 +2,21 @@ extends Label
 
 var count = 5
 var timer
+var tween
 
 func _ready():
+	tween = get_node("Tween")
+	tween.interpolate_property(self, "modulate:a", 1, 0, 1, Tween.TRANS_QUAD)
+	tween.set_repeat(true)
+	
 	timer = Timer.new()
 	add_child(timer)
 	timer.connect("timeout", self, "tick")
 	timer.set_wait_time(1)
 	timer.set_one_shot(false)
 	timer.start()
+	
+	tween.start()
 
 func tick():
 	count -= 1
