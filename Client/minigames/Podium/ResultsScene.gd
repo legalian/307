8237c11b2b7bd3f7bf16a98bc2c2b8b2a4a7bf02
WilliamNewt <40041126+WilliamNewt@users.noreply.github.com>
2021-03-11 13:47:44 -0500

    Coded Podium Scene

--- Client/minigames/Podium/ResultsScene.gd
+++ Client/minigames/Podium/ResultsScene.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
-var Serverlist = []
-var Playerlist = []
-var PlayerData = ["Name", 0, 1]
+onready var generalserver = get_node("/root/Server")
+var Playerlist = generalserver.players
+var SelfPlayer = generalserver.selfplayer
+var TopPlayers = Playerlist
+
+var AvatarStyles = ["Racoon"]
+var HatStyles = ["None","Tophat","Smallhat","Viking","Paperhat","Headphones"]
+var HatLocation = [null, 
+"res://Hats/Tophat.tscn",
+"res://Hats/Whitehat.tscn",
+"res://Hats/Viking.tscn",
+"res://Hats/Paperhat.tscn",
+"res://Hats/Headphones.tscn"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	_SortPlayers()
 	_SetScene()
 
 
@@ -14,5 +25,10 @@ func _SetScene():
 	var currentPlacement = null
 	for i in range(1,4):
 		currentPlacement = find_node("Place" + str(i))
-		currentPlacement.get_node("PlayerName").bbcode_text = "[center]" + PlayerData[0] + "[/center]"
+		currentPlacement.get_node("PlayerName").bbcode_text = "[center]" + TopPlayers[i-1].username + "[/center]"
+		currentPlacement.get_node("Avatar").set_Hat(HatLocation[TopPlayers[i-1].hat])
 		
+
+func _SortPlayers():
+	print("Sort Players")
+	
