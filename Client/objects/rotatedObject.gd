492df5f00b5b9b6f23d0f35c8e7bfb23e8e71724
Parker Lawrence <liverspleenkidney@gmail.com>
2021-03-11 15:52:13 -0500

    fence fix

--- Client/objects/rotatedObject.gd
+++ Client/objects/rotatedObject.gd
@@ -9,11 +9,12 @@ func _process(delta):
 	if !$VisibilityNotifier.is_on_screen(): return
 	#set_rotation(get_rotation() + delta * 5)
 	var glt = get_global_transform_with_canvas()
-	var gltp = atan2(glt[0][1]/.44,glt[0][0])
-	$Visible/Sprite.frame = 63-(int(64+64*gltp/(2*PI))%64);
+	var gltp = atan2(glt[0][1],glt[0][0]*.2)
+	$Visible/Sprite.frame = 63-(int(round(64+64*gltp/(2*PI)))%64);
 	
 	glt.origin = Vector2(0,0);
 	var invscale = 1/(glt[0][0]*glt[1][1]-glt[1][0]*glt[0][1])
+	print(invscale)
 	glt = glt.scaled(Vector2(invscale,invscale))
 	$Visible.transform = glt.affine_inverse();
 
