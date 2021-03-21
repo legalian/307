d418cb5acf139dd8f5f3fcb88d485528015ca2f2
WilliamNewt <40041126+WilliamNewt@users.noreply.github.com>
2021-03-21 16:29:33 -0400

    Random Default Name list

--- Client/scripts/MinigameServers/Player.gd
+++ Client/scripts/MinigameServers/Player.gd
@@ -1,15 +1,19 @@
 extends Node
 
+var randomNameList = ["Axe","Bean","Bear","Bee","Bird","Cactus","Burger","Claw","Coyote","Crumb","Dragon","Diamond","Ghost","Fox","Gamer","Joker","Noodle","Wolf","Keibo","Tomato","Storm","Socks","Lizard","Undefined","Lost","OnePiece","Cris","UwU","BigBoi","NoName"]
+
 var playerID
 var score = 0
-var username = ""
+var username = "" 
 var avatar = 0
 var hat = 0
 var vehicle = 0
 
 func _init(var packed):
 	playerID = packed['id']
-	username = packed.get('username',"User "+str(packed['id']))
+	#username = packed.get('username',"User "+str(packed['id']))
+	username = packed.get('username',randomUsername())
+
 	avatar = packed.get('avatar',0)
 	score = packed.get('score',0)
 	hat = packed.get('hat',0)
@@ -31,3 +35,10 @@ func pack():
 		'username':username,
 		'vehicle':vehicle
 	}
+
+
+func randomUsername():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var my_random_number = rng.randi_range(0, randomNameList.size()-1)
+	return randomNameList[my_random_number]
