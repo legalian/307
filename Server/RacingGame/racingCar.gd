992e0d111cb0dbf44726a277c6e430a5b1f53f2f
Chen Kai Chuang <chuang23@purdue.edu>
2021-03-28 18:29:28 -0400

    add trap functionality, re-add in keybinds

--- Server/RacingGame/racingCar.gd
+++ Server/RacingGame/racingCar.gd
@@ -68,6 +68,8 @@ func _physics_process(delta):
 		if collision.collider.name.begins_with("Powerup"):
 			collision.collider.pickup(self)
 			break;
+		if collision.collider.name.begins_with("Trap"):
+			stun(4, 500)
 
 func _process(delta):
 	if input_dict["usingPowerup"] and cur_powerup != null:
@@ -97,6 +99,8 @@ func interrupt():
 
 func stun(duration, speedMax):
 	# Somehow stop the car's movements.
+	cleanse();
+	# Cleanse at start of stun, to prevent multiple effects from causing issues. 
 	maxSpeed = speedMax;
 	if(speed > maxSpeed): speed = maxSpeed;
 	var timer = Timer.new()
