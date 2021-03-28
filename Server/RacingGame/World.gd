51140c82acfc69e00a447ba50b167cda2757b1c8
lmeng1214 <lmeng1214@gmail.com>
2021-03-27 20:48:22 -0400

    Fixed racing allowing movement at 2 sec mark

--- Server/RacingGame/World.gd
+++ Server/RacingGame/World.gd
@@ -65,7 +65,7 @@ func _ready():
 	var _countdown = Timer.new()
 	add_child(_countdown)
 	_countdown.connect("timeout", self, "_countdown_end")
-	_countdown.set_wait_time(6.5)
+	_countdown.set_wait_time(8.5)
 	_countdown.set_one_shot(true)
 	_countdown.start()
 	
