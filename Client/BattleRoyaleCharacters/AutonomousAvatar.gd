ac7f323a920a7333db5a4eb9014165a57dd5c55c
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-01 20:09:17 -0400

    hotfix

--- Client/BattleRoyaleCharacters/AutonomousAvatar.gd
+++ Client/BattleRoyaleCharacters/AutonomousAvatar.gd
@@ -11,11 +11,9 @@ var dying = false
 var server = null
 var l_gun = -1
 var l_body = 0
-var body
 
 func _ready():
 	server = get_node("/root/Server").get_children()[0]
-	body = get_node("Body")
 
 
 func setAvatar(index):
@@ -49,13 +47,13 @@ func _physics_process(delta):
 	rotate(rotvel*delta)
 
 func damage():
-	body.ouch()
+	$Body.ouch()
 	
 func die():
 	dying = true
 	velocity = Vector2.ZERO
 	get_node("CollisionShape2D").disabled = true#disable collisions and begin dying
-	body.rip()
+	$Body.rip()
 	var _timer = Timer.new()
 	add_child(_timer)
 	_timer.connect("timeout", self, "finish_dying")
