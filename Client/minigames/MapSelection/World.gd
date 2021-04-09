c95904ee3224086a765798c103a656cfeb9f938a
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-09 14:29:46 -0400

    made it so shims do not trigger game and map selection screens

--- Client/minigames/MapSelection/World.gd
+++ Client/minigames/MapSelection/World.gd
@@ -42,7 +42,10 @@ func _process(delta):
 			wheel.rotation_degrees = frame;
 			curSpin = curSpin - 1
 			spinCount = spinCount - 1
-	if(spinSet == 1 && curSpin <= 0):
+	
+	var early = OS.get_environment("MULTI_USER_TESTING")!=null and "shim" in OS.get_environment("MULTI_USER_TESTING")
+
+	if early or (spinSet == 1 && curSpin <= 0):
 		done = true;
 		self.get_node("World/Camera2D").current = false;
 		self.visible = false;
