87d734eb96068039951648884d3cb55b562d8de6
WilliamNewt <40041126+WilliamNewt@users.noreply.github.com>
2021-04-11 19:05:15 -0400

    Setting Loading and Saving

--- Client/Menus/Settings.gd
+++ Client/Menus/Settings.gd
@@ -1,21 +1,36 @@
 extends Node
 
+onready var generalserver = get_node("/root/Server")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var MasterVolume
+var MusicVolume
+var SFXVolume
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	MasterVolume = generalserver.selfplayer.volume[0]
+	MusicVolume = generalserver.selfplayer.volume[1]
+	SFXVolume = generalserver.selfplayer.volume[2]
+	_SetVolumeText()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _SetVolumeText():
+	find_node("Master Volume").get_node("SettingValue").bbcode_text  = "[right]" + str(MasterVolume) + "%"
+	find_node("Music Volume").get_node("SettingValue").bbcode_text  = "[right]" + str(MusicVolume) + "%"
+	find_node("SFX Volume").get_node("SettingValue").bbcode_text  = "[right]" + str(SFXVolume) + "%"
 
 func _on_BackButton_pressed():
 	AudioPlayer.play_sfx("res://audio/sfx/click_002.ogg")
 	var _success = get_tree().change_scene("res://Main.tscn")
+
+
+func _on_MasterRandomize_pressed():
+	AudioPlayer.play_sfx("res://audio/sfx/click_002.ogg")
+	var newValue = randomNumber()
+	generalserver.selfplayer.volume[0] = newValue
+	MasterVolume = generalserver.selfplayer.volume[0]
+	_SetVolumeText()
+
+func randomNumber():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var randomValueNum = rng.randi_range(0, 100)
+	return randomValueNum
