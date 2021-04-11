26566bf071a0fe0374d86a75bfb60d3ab4556558
Chen Kai Chuang <chuang23@purdue.edu>
2021-04-11 18:00:27 -0400

    reverted accidental changes to racing game coundown

--- Client/minigames/RacingGame/Countdown.gd
+++ Client/minigames/RacingGame/Countdown.gd
@@ -1,30 +1,32 @@
 extends Label
 
-var time;
+var count = 5
+var timer
 var tween
 
-# Server sets the time, not the client. Since unlike racing or battle royale, time is much more important in this game. 
-
 func start():
+	timer.start()
+	
 	tween.start()
 
 func _ready():
 	tween = get_node("Tween")
 	tween.interpolate_property(self, "modulate:a", 1, 0, 1, Tween.TRANS_QUAD)
 	tween.set_repeat(true)
-
-#	timer = Timer.new()
-#	add_child(timer)
-#	timer.connect("timeout", self, "tick")
-#	timer.set_wait_time(1)
-#	timer.set_one_shot(false)
 	
-	
+	timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", self, "tick")
+	timer.set_wait_time(1)
+	timer.set_one_shot(false)
 
 
-func setTime(times):
-	time = times
-	if time < 0:
-		set_text("Round End!");
+func tick():
+	count -= 1
+	if count == 0:
+		set_text("GO!")
+	elif count < 0:
+		timer.stop()
+		set_visible(false)
 	else:
-		set_text(str(time)+"!")
+		set_text(str(count))
