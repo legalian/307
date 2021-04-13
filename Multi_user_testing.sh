5db28777db225c2a8954018b08cdae68e5563896
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-13 11:35:46 -0400

    improved testing script, gave the normal one linux support so there doesn't need to be two anymore, and improved support for choosing powerup and map

--- Multi_user_testing.sh
+++ Multi_user_testing.sh
@@ -1,7 +1,6 @@
 acceptableTests=('party' 'lobby' 'quickplay' 'podium' 'battleroyale' 'racing' 'demoderby' 'confusingcaptcha' 'battleroyale_shim' 'racing_shim' 'demoderby_shim' 'podium_shim' 'confusingcaptcha_shim')
 associatedCount=('4'     '4'     '4'         '4'      '2'            '2'      '2'         '2'									'1'                 '1'           '1'              '1'          '1')
 
-#use "nonpowerups", "nonmap" to not make a choice
 powerups=('speed' 'missile' 'trap', 'nonpowerups')
 maps=('desert' 'grassland' 'nonmap')
 
@@ -9,35 +8,54 @@ MAPTEST="nonmap"
 POWERUPTEST="nonpowerups"
 
 DESIREDSCREEN=1
+
+print_usage () {
+	echo "Usage: Multi_user_testing.sh test [desiredmap] [desiredpowerup] [desiredscreen]"
+	echo "test must be one of:"
+	echo "	${acceptableTests[*]}"
+	echo "desiredmap must be one of:"
+	echo "	${maps[*]}"
+	echo "desiredpowerup must be one of:"
+	echo "	${powerups[*]}"
+	exit 1
+}
+
 if [[ $# == 0 ]]; then
 	echo "Not enough arguments"
-	echo "Usage: Multi_user_testing.sh test [desiredscreen]"
-	echo "test must be one of:"
-	echo "${acceptableTests[*]}"
-	exit 1
+	print_usage
 fi
-if ! printf '%s\n' "${acceptableTests[@]}" | grep -q "^$1$"; then
-	echo "Invalid test"
-	echo "Usage: Multi_user_testing.sh test [desiredscreen]"
-	echo "test must be one of:"
-	echo "${acceptableTests[*]}"
-	exit 1
+if [[ $# -ge 2 ]]; then
+	MAPTEST=$2
 fi
-
+if [[ $# -ge 3 ]]; then
+	POWERUPTEST=$3
+fi
 if [[ $# == 4 ]]; then
 	DESIREDSCREEN=$4
 fi
+if ! printf '%s\n' "${acceptableTests[@]}" | grep -q "^$1$"; then
+	echo "Invalid test"
+	print_usage
+fi
+if ! printf '%s\n' "${maps[@]}" | grep -q "^$MAPTEST$"; then
+	echo "Invalid map"
+	print_usage
+fi
+if ! printf '%s\n' "${powerups[@]}" | grep -q "^$POWERUPTEST$"; then
+	echo "Invalid powerup"
+	print_usage
+fi
+
+if [[ -f /Applications/Godot.app/Contents/MacOS/Godot ]]
+then
+	gdpath="/Applications/Godot.app/Contents/MacOS/Godot"
+else
+	gdpath="godot"
+fi
+
 
 cd Server
-if [[ $# -ge 2 ]]; then
-	MAPTEST=$2;
-	echo $MAPTEST;
-fi
-if [[ $# -ge 3 ]]; then
-	POWERUPTEST=$3;
-	echo $POWERUPTEST
-fi
-MULTI_USER_TESTING=$1 DESIREDSCREEN="$DESIREDSCREEN" POWERUPTEST=$POWERUPTEST MAPTEST=$MAPTEST /Applications/Godot.app/Contents/MacOS/Godot &
+MULTI_USER_TESTING=$1 DESIREDSCREEN="$DESIREDSCREEN" POWERUPTEST=$POWERUPTEST MAPTEST=$MAPTEST $gdpath &
 cd ..
 sleep 2
 for i in "${!acceptableTests[@]}"; do
@@ -47,5 +65,5 @@ for i in "${!acceptableTests[@]}"; do
 done
 cd Client
 for ((i=1;i<=END;i++)); do
-    MULTI_USER_TESTING=$1 ACTIVECORNER=$i DESIREDSCREEN="$DESIREDSCREEN" /Applications/Godot.app/Contents/MacOS/Godot -t &
+    MULTI_USER_TESTING=$1 ACTIVECORNER=$i DESIREDSCREEN="$DESIREDSCREEN" $gdpath -t &
 done
