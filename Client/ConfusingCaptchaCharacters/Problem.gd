4979501a62bd0dab7d52f4b44a6473b847715cc0
Chen Kai Chuang <chuang23@purdue.edu>
2021-04-16 17:27:12 -0400

    add sha256, calculus captchas

--- Client/ConfusingCaptchaCharacters/Problem.gd
+++ Client/ConfusingCaptchaCharacters/Problem.gd
@@ -4,7 +4,10 @@ var questions = [
 	"Pick the motorcycle",
 	"Which of these programs has 38 file descriptors open at the end of execution, including stdin, stdout, and stderr",
 	"Which hair resembles an exponential equation?",
-	"Which number shows the number, 1?"
+	"Which number shows the number, 1?",
+	"Reverse the following SHA-256 hash: 2c0f0f4581aa813bfe2ea2128451ab86cee6b7442e17879e939751c9aa893859",
+	"Find the surface area of the parametric surface r(u,v)=(2u+3v, 3u+v, 2), 0<=u<=2, 0<=v<=1",
+	"What is the correct R code to generate the p value?"
 ]
 var roundEnded = false;
 
