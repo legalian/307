d6c97f88d5a82cb5cf6adda2204c974fa8c3ff84
lmeng1214 <lmeng1214@gmail.com>
2021-04-21 17:18:58 -0400

    Added lobby min checking for leave party and updated Racing UI #time 1h

---
+++ Client/lose.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+func _ready():
+	AudioPlayer.pause_music()
+	AudioPlayer.play_sfx("res://audio/sfx/wrong.ogg")
+
+func _on_LeaveGame_pressed():
+	AudioPlayer.play_sfx("res://audio/sfx/click_002.ogg")
+	Server.leave_party()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	AudioPlayer.play_music("res://audio/music/mainmenu" + str(rng.randi_range(1,2)) + ".ogg")
+	get_tree().change_scene("res://Main.tscn")
