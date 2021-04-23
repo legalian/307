bdcc865cd6c3866d13a3a834b73da0aa037860f3
evantygf <evan.dunning@gmail.com>
2021-04-23 00:42:05 -0400

    Made missile powerup larger

--- Client/minigames/RacingGame/objects/PU_Proj.gd
+++ Client/minigames/RacingGame/objects/PU_Proj.gd
@@ -1,11 +1,12 @@
+tool
 extends "res://objects/rotatedObject.gd"
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
-	pass
+	._process(delta)
 
 func unpack(package):
 	position = Vector2(package['x'],package['y'])
-	rotation = package['r']
+	rotation = package['r'] + 3.0*PI/2.0
