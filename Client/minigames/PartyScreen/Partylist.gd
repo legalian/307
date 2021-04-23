d2df23132b57b61267ace4b896357764f4d3d890
evantygf <evan.dunning@gmail.com>
2021-04-23 02:29:52 -0400

    Sorted player usernames in party screen

--- Client/minigames/PartyScreen/Partylist.gd
+++ Client/minigames/PartyScreen/Partylist.gd
@@ -8,8 +8,13 @@ func update_playerlist():
 	print("updated!")
 	for player_row in get_children():
 		player_row.queue_free()
+	var usernames = []
 	for player in Server.players:
+		usernames.append(str(player.username))
+	usernames.sort()
+	for n in usernames:
 		var player_ID = Label.new()
-		player_ID.text = str(player.username)
+		player_ID.text = n
 		player_ID.add_color_override("font_color", Color(0, 1, 1, 1))
 		add_child(player_ID)
+
